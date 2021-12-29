93644f01626531c6f22d36a70f714e692ea8b88e
John Mark <johnmark.almera.a@bulsu.edu.ph>
2021-12-30 00:30:00 +0800

    Archive Update
    updated to prepared statements

--- php/patients_record_archive/restore.php
+++ php/patients_record_archive/restore.php
@@ -9,15 +9,17 @@ if(isset($_GET['id']))
     $patient_id = $_GET['id'];
     rename("../../archive/".$patient_id,"../../patient/".$patient_id);
 
-    //set record status
-    $updateStat = "UPDATE tbl_patients SET record_status = 'Active' WHERE patient_id = '".$patient_id."';";
-    mysqli_query($conn,$updateStat);
-    header('Location: patients_archive.php');
+    $stmt = $connection->prepare("UPDATE tbl_patients SET record_status = 'Active' WHERE patient_id = ?;");
 
+    /* Prepared statement, stage 2: bind and execute */
+    $stmt->bind_param("s", $patient_id); 
+
+    $stmt->execute();
+    header('Location: ../patients_record_archive/patients_archive.php');
 }
 
 else{
-    header('Location: patients_archive.php');
+    header('Location: ../patients_record_archive/patients_archive.php');
 }
 
 
--- php/patients_record_management/archive_folder.php
+++ php/patients_record_management/archive_folder.php
@@ -9,11 +9,12 @@ if(isset($_GET['id']))
     $patient_id = $_GET['id'];
     rename("../../patient/".$patient_id, "../../archive/".$patient_id);
 
+    $stmt = $connection->prepare("UPDATE tbl_patients SET record_status = 'Archive' WHERE patient_id = ?;");
 
+    /* Prepared statement, stage 2: bind and execute */
+    $stmt->bind_param("s", $patient_id); 
 
-    //set record status
-    $updateStat = "UPDATE tbl_patients SET record_status = 'Archive' WHERE patient_id = '".$patient_id."';";
-    mysqli_query($conn,$updateStat);
+    $stmt->execute();
     header('Location: patients_records.php');
 }
 else{
