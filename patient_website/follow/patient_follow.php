9f9e77ab49f5f48e7f29ab454b31f69f30362658
John Mark <johnmark.almera.a@bulsu.edu.ph>
2022-01-05 22:32:39 +0800

    Update Patient website (Follow up result)

--- patient_website/follow/patient_follow.php
+++ patient_website/follow/patient_follow.php
@@ -17,7 +17,195 @@
 
   <?php include_once '../nav/patient_header.php' ?>
 
-    follow
+  <div class="row justify-content-center my-3">
+    <div class="col-12 col-sm-8 p-2">
+
+        
+        <div class="row">
+          <div class="col text-center">
+            <h3>Select Results</h3>
+          </div>
+        </div>
+
+        <form>
+            <!-- div for check results -->
+            <div class="row border rounded m-2">
+              <div class="col-12">
+
+                <!-- check row  -->
+                <div class="row">
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="cbc" value="cbc">
+                      <label class="form-check-label" for="cbc">
+                        Complete Blood Count (CBC)
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="platelet" value="platelet">
+                      <label class="form-check-label" for="platelet">
+                        Platelet Count
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="blood_typing" value="blood_typing">
+                      <label class="form-check-label" for="blood_typing">
+                        Blood Typing
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                </div>
+                <!-- check row end-->
+
+                <!-- check row  -->
+                <div class="row">
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="cross_matching" value="cross_matching">
+                      <label class="form-check-label" for="cross_matching">
+                        Cross Matching
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="hepatitis_b" value="hepatitis_b">
+                      <label class="form-check-label" for="hepatitis_b">
+                        Hepatitis B
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="bun" value="bun">
+                      <label class="form-check-label" for="bun">
+                        Blood Urea Nitrogen (BUN)
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                </div>
+                <!-- check row end-->
+
+                <!-- check row  -->
+                <div class="row">
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="creatinine" value="creatinine">
+                      <label class="form-check-label" for="creatinine">
+                        Creatinine
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="fbs" value="fbs">
+                      <label class="form-check-label" for="fbs">
+                        Fasting Blood Sugar (FBS)
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="fecalysis" value="fecalysis">
+                      <label class="form-check-label" for="fecalysis">
+                        Fecalysis
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                </div>
+                <!-- check row end-->
+
+                <!-- check row  -->
+                <div class="row">
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="cholesterol" value="cholesterol">
+                      <label class="form-check-label" for="cholesterol">
+                        Cholesterol
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="uric_acid" value="uric_acid">
+                      <label class="form-check-label" for="uric_acid">
+                        Uric Acid
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                  <div class="col-12 col-sm-4 p-2">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="urinalysis" value="urinalysis">
+                      <label class="form-check-label" for="urinalysis">
+                        Urinalysis
+                      </label>
+                    </div>
+                  
+                  </div>
+
+                </div>
+                <!-- check row end-->
+
+                 <!-- follow submit  -->
+                 <div class="row justify-content-center">
+                    <div class="col-12 col-sm-4 p-2">
+                        <input class="form-control" type="submit" name="follow" value="Follow Up">
+                    </div>
+                </div>
+                <!-- follow submit end-->
+
+              </div>
+            </div>
+            
+            <!-- div for check results end -->
+        </form>
+        
+
+
+    </div>
+  </div>
 
 
                                         
